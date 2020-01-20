32d7e177aa8f1983b6d7f0791b1287a55d7e3c8f
Seth Hall <seth@ushahidi.com>
2020-01-20 16:53:28 -0500

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,33 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import '../scss/styles.scss'
 
 const IndexPage = () => (
+  // <Layout>
+  //   <SEO title="Home" />
+  //   <h1>Hi people</h1>
+  //   <p>Welcome to your new Gatsby site.</p>
+  //   <p>Now go build something great.</p>
+  //   <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+  //     <Image />
+  //   </div>
+  //   <Link to="/page-2/">Go to page 2</Link>
+  // </Layout>
+
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="container">
+      <div className="columns">
+        <div className="column">
+          <h2 className="title is-2">Level 2 heading</h2>
+          <p className="content">Cool content. Using Bulma!</p>
+        </div>
+        <div className="column is-four-fifths">
+          <h2 className="title is-2">Level 2 heading</h2>
+          <p className="content">This column is cool too!</p>
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
