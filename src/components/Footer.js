f94aa9e37ed8b6d4da9c728e68c6ac2b32724dc3
Seth Hall <seth@ushahidi.com>
2020-01-20 23:59:54 -0500

    footer revisions

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faInstagram } from "@fortawesome/free-brands-svg-icons"
 
 const Footer = () => {
   return (
@@ -9,6 +11,9 @@ const Footer = () => {
             <strong>Hall Pictures</strong> is a film production company based
             out of Greensboro, NC.{" "}
             <a href="mailto:seth@hallpictures.us">Say Hello!</a>
+            <a href="https://www.instagram.com/sethburtonhall/" target="_blank">
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
           </p>
         </div>
         <div className="copyright">
