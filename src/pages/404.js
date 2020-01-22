b71d8019a4dc32a0e095d233296ac401241ed62f
Seth Hall <seth@ushahidi.com>
2020-01-22 08:26:05 -0500

    styles for 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 import gif from "../images/404-2.gif"
+import "../scss/styles.scss"
 
 const NotFoundPage = () => (
   <Layout>
