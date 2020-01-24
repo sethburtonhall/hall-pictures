7f0143e070b0480e6d1e5c87f3b4564af8686fd4
Seth Hall <seth@ushahidi.com>
2020-01-23 20:34:10 -0500

    moved json data to one file, exported named modules, imported

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,57 +3,10 @@ import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 import Bio from "../components/Bio"
 import Pipeline from "../components/Pipeline"
+import { pipeline } from "../data/data"
 import "../scss/styles.scss"
 
 const About = () => {
-  const pipeline = [
-    {
-      stage: "Development",
-      projects: [
-        {
-          name: "Rubber Chicken",
-          type: "web series",
-          genre: "comedy",
-        },
-        {
-          name: "8am",
-          type: "short film",
-          genre: "drama",
-        },
-      ],
-    },
-    {
-      stage: "Pre-Production",
-      projects: [
-        {
-          name: "Liquidity",
-          type: "short film",
-          genre: "horror",
-        },
-      ],
-    },
-    {
-      stage: "Production",
-      projects: [
-        {
-          name: "Liquidity",
-          type: "April",
-          genre: "2020",
-        },
-      ],
-    },
-    {
-      stage: "Post Production",
-      projects: [
-        {
-          name: "Morning Routine",
-          type: "short film",
-          genre: "drama",
-        },
-      ],
-    },
-  ]
-
   return (
     <Layout>
       <SEO title="About" />
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,58 +2,14 @@ import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 import Video from "../components/Video"
-import spotlight from "../images/laurels/spotlight.jpg"
-import dumbo from "../images/laurels/dumbo.jpg"
-import riverrun from "../images/laurels/riverrun.jpg"
-import strangerDays from "../images/laurels/stranger-days.png"
-import atlantaComedy from "../images/laurels/atlanta-comedy.jpg"
+import { films } from "../data/data"
 import "../scss/styles.scss"
 
 const IndexPage = () => {
-  const videos = [
-    // The video title should be all lower case and include dashes between words.
-    // This will allow for '.title-name' styling in 'video.scss'
-    // and 'Video.js' will replace '-' with spaces to display title correctly
-    {
-      title: "tropical-gardens",
-      logline:
-        "A down and out loner copes with his recent heartbreak by obsessing over a mini-golf course record.",
-      director: "Morten Savage",
-      producer: "Seth Hall",
-      url: "https://player.vimeo.com/video/327592520",
-      hasLaurels: true,
-      laurels: [
-        {
-          image: spotlight,
-        },
-        {
-          image: dumbo,
-        },
-        {
-          image: riverrun,
-        },
-        {
-          image: strangerDays,
-        },
-        {
-          image: atlantaComedy,
-        },
-      ],
-    },
-    {
-      title: "circles",
-      logline:
-        "A frail and helpless 18-year-old finds himself in a wilderness rehabilitation program with a second chance to make his way in the world.The problem is that he refuses to get out of his tent.",
-      director: "Morten Savage",
-      producer: "Seth Hall",
-      url: "https://player.vimeo.com/video/284174147",
-      hasLaurels: false,
-    },
-  ]
   return (
     <Layout>
       <SEO title="Home" />
-      {videos.map((video, index) => (
+      {films.map((video, index) => (
         <Video
           key={index}
           title={video.title}
