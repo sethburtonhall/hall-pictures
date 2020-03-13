cc17afa61e1c1170eef80ef0cc1222902631cff5
Seth Hall <seth@ushahidi.com>
2020-03-13 16:06:50 -0400

    adds HP icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Hall Pictures`,
     description: `Film Production`,
-    author: `@sethburton`
+    author: `@sethburton`,
   },
   plugins: [
     `gatsby-plugin-sass`,
@@ -31,7 +31,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/hallpictures-icon.jpg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
